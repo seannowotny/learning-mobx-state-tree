32f3e9e63cff1fffa6c5fdf570a65bff74e4b28a
SNx42 <seannowotny@live.de>
2019-11-25 13:30:56 +1000

    Remove mobx-devtools-mst

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import { onPatch } from 'mobx-state-tree';
-import makeInspectable from 'mobx-devtools-mst';
 import { invoice } from './models/Invoice';
 import { connectReduxDevtools } from "mst-middlewares"
 
@@ -13,7 +12,6 @@ connectReduxDevtools(require("remotedev"), invoice);
 onPatch(invoice, patch => {
    console.log(patch);
 });
-makeInspectable(invoice);
 
 ReactDOM.render(
    // <Provider
