4d6adc7620d79065244a4f8693f5ad000960fdcc
SNx42 <seannowotny@live.de>
2019-11-26 11:30:03 +1000

    Make basic working version

--- src/components/App.js
+++ src/components/App.js
@@ -2,35 +2,36 @@
 import React, { useContext, SyntheticEvent } from 'react';
 import './App.css';
 import { observer } from 'mobx-react-lite';
-import { InvoiceContext } from '../models/Invoice';
-import Item from './Item';
-import { Item as ItemModel, ItemType } from '../models/Item';
+// import { InvoiceContext } from '../models/Invoice';
+// import Item from './Item';
+// import { Item as ItemModel, ItemType } from '../models/Item';
 
 export default observer(() =>
 {
-  const invoice = useContext(InvoiceContext);
+  // const invoice = useContext(InvoiceContext);
 
-  const handleSubmit = (e: any) =>
-  {
-    e.preventDefault();
+  // const handleSubmit = (e: any) =>
+  // {
+  //   e.preventDefault();
 
-    const name = e.target.name.value;
-    const quantity = e.target.quantity.value;
-    const price = e.target.price.value;
+  //   const name = e.target.name.value;
+  //   const quantity = e.target.quantity.value;
+  //   const price = e.target.price.value;
 
-    const itemModel = ItemModel.create({
-      name: name,
-      quantity: parseInt(quantity, 10),
-      price: parseFloat(price)
-    });
-    invoice.itemList.add(itemModel);
-    e.target.reset();
-    e.target.name.focus();
-  }
+  //   const itemModel = ItemModel.create({
+  //     name: name,
+  //     quantity: parseInt(quantity, 10),
+  //     price: parseFloat(price)
+  //   });
+  //   invoice.itemList.add(itemModel);
+  //   e.target.reset();
+  //   e.target.name.focus();
+  // }
 
   return (
     <div className="App">
-      <h1>{invoice.status}</h1>
+      <h1>Test</h1>
+      {/* <h1>{invoice.status}</h1>
       {! invoice.is_paid &&
         <button onClick={() => invoice.markPaid()}>Pay</button>
       }
@@ -60,7 +61,7 @@ export default observer(() =>
         {invoice.itemList.items.map((item: ItemType, i: number) => (
           <Item item={item} key={i} />
         ))}
-      </ul>
+      </ul> */}
     </div>
   );
 });
--- src/index.js
+++ src/index.js
@@ -3,24 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-import { onPatch } from 'mobx-state-tree';
-import { invoice } from './models/Invoice';
-import { connectReduxDevtools } from "mst-middlewares"
 
-connectReduxDevtools(require("remotedev"), invoice);
-
-onPatch(invoice, patch => {
-   console.log(patch);
-});
-
-ReactDOM.render(
-   // <Provider
-   // invoice={invoice}
-   // >
-      <App/>,
-   // </Provider>
-   document.getElementById('root')
-);
+ReactDOM.render(<App/>,document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
