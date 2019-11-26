eae94f9ca69dc3d13cd4e42560af714b8583e8df
SNx42 <seannowotny@live.de>
2019-11-26 15:30:46 +1000

    mobx-remotedv problem

--- src/stores/rootStore.js
+++ src/stores/rootStore.js
@@ -1,12 +1,24 @@
 // @flow
 
+import remotedev from 'mobx-remotedev';
+
 import { observable } from 'mobx';
 
 import Invoice from './Invoice';
 
-export class RootStore
+@remotedev class RootStoreClass
 {
    @observable invoice: Invoice = new Invoice(/*this*/);
 }
 
+export const RootStore = RootStoreClass;
+
 export const rootStore = new RootStore();
+
+
+// export class RootStore
+// {
+//    @observable invoice: Invoice = new Invoice(/*this*/);
+// }
+
+// export const rootStore = remotedev(new RootStore());
