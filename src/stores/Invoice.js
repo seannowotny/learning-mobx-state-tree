a465b0ef128b658c3d38605191f83e0a2072921d
seannowotny <seannowotny@live.de>
2019-11-29 14:58:33 +1000

    Remove unused rootStore import

--- src/stores/Invoice.js
+++ src/stores/Invoice.js
@@ -2,18 +2,10 @@
 
 import { action, observable } from 'mobx';
 import ItemList from './models/ItemList';
-import { RootStore } from './rootStore';
 import { persist } from 'mobx-persist';
 
 export default class Invoice
-{
-   // constructor(rootStore: RootStore)
-   // {
-   //    this.rootStore = rootStore;
-   // }
-
-   // @observable rootStore: RootStore;
-   
+{  
    @persist @observable currency: string = 'CAD';
    @persist @observable isPaid: boolean = false;
    @persist('object', ItemList) @observable itemList: ItemList = new ItemList();
