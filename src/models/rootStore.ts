96659f09ff03c2be55b5af61b63263cd990e160e
SNx42 <seannowotny@live.de>
2019-11-29 15:32:14 +1000

    Persist invoice

--- src/models/rootStore.ts
+++ src/models/rootStore.ts
@@ -1,6 +1,7 @@
 import { types } from 'mobx-state-tree';
 import { createContext } from 'react';
 import { Invoice } from './Invoice';
+import { persist } from 'mst-persist';
 
 const RootStore = types
    .model('RootStore', {
@@ -10,3 +11,9 @@ const RootStore = types
 export const rootStore = RootStore.create({ invoice: Invoice.create({ currency: 'CAD' }) });
 export const rootStoreContext = createContext(rootStore);
 export const invoiceContext = createContext(rootStore.invoice);
+
+persist('rootStore', rootStore, {
+   storage: localStorage,
+   jsonify: true,
+   whitelist: ['invoice']
+ }).then(() => console.log('someStore has been hydrated'));
